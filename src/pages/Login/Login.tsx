61524706d4d6231a185e19da77822c5b864c2254
IvannEmmanuel <F0dG1ngD13@gmail.com>
2025-09-18 21:05:55 +0800

    refactor: added login

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,127 @@
+import { StyleSheet, Text, View, SafeAreaView, ImageBackground, Image, TextInput, TouchableOpacity } from 'react-native'
+import React from 'react'
+
+const Login = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <ImageBackground
+        source={require('../../../assets/loginBackground.png')}
+        style={styles.loginBackground}
+        resizeMode='cover'>
+        <View>
+          <Text style={styles.loginText}>Login</Text>
+          <View style={{ alignSelf: 'center', top: 40 }}>
+            <Image source={require('../././../../assets/logo.png')} style={styles.logo} />
+            <Text style={styles.rentAllText}>RentAll</Text>
+          </View>
+        </View>
+        <View style={{ paddingHorizontal: 20, top: 150 }}>
+          <Text style={styles.text}>Email</Text>
+          <TextInput
+            placeholder="juan@gmail.com"
+            placeholderTextColor='#bebebeff'
+            style={styles.textInput}
+          />
+          <Text style={styles.text}>Password</Text>
+          <TextInput
+            placeholder="Password"
+            placeholderTextColor='#bebebeff'
+            style={styles.textInput}
+            secureTextEntry
+          />
+          <TouchableOpacity style={styles.loginButton}>
+            <Text style={styles.loginButtonText}>Login</Text>
+          </TouchableOpacity>
+          <View style={{ top: 40 }}>
+            <View style={styles.bottomContainer}>
+              <TouchableOpacity>
+                <Text style={styles.forgotPasswordText}>
+                  Forgot Password?
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.accountContainer}>
+              <Text style={styles.accountText}>Don't have an account?</Text>
+              <TouchableOpacity>
+                <Text style={styles.signUpText}>
+                  Sign Up
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </ImageBackground>
+    </SafeAreaView>
+  )
+}
+
+export default Login
+
+const styles = StyleSheet.create({
+  loginBackground: {
+    flex: 1,
+    top: 30
+  },
+  loginText: {
+    color: 'white',
+    fontSize: 32,
+    paddingLeft: 30,
+    fontFamily: 'DM-Bold'
+  },
+  logo: {
+    width: 120,
+    height: 100,
+    alignSelf: 'center'
+  },
+  rentAllText: {
+    fontFamily: 'FugazOne-Regular',
+    fontSize: 40,
+    color: 'white',
+  },
+  text: {
+    paddingLeft: 10,
+    marginBottom: 3
+  },
+  textInput: {
+    height: 50,
+    borderRadius: 12,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    color: 'black',
+    paddingLeft: 20,
+    marginBottom: 20,
+  },
+  loginButton: {
+    width: '100%',
+    height: 50,
+    borderRadius: 20,
+    backgroundColor: '#1E1E1E',
+    justifyContent: 'center',
+    top: 20,
+  },
+  loginButtonText: {
+    color: 'white',
+    alignSelf: 'center',
+    fontSize: 15
+  },
+  bottomContainer: {
+    flexDirection: 'row'
+  },
+  forgotPasswordText: {
+    fontSize: 12,
+    marginBottom: 10
+  },
+  accountContainer: {
+    flexDirection: 'row'
+  },
+  accountText: {
+    fontSize: 12,
+  },
+  signUpText: {
+    paddingLeft: 5,
+    fontSize: 12,
+    fontWeight: '700',
+    color: '#FFAB00'
+  }
+})
