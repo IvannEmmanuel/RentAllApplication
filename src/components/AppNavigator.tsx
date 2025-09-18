3565236063e85348b84beea9871704350e889cee
IvannEmmanuel <F0dG1ngD13@gmail.com>
2025-09-18 21:05:30 +0800

    refactor: added appnavigator

---
+++ src/components/AppNavigator.tsx
@@ -0,0 +1,17 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LandingPage from '../pages/LandingPage';
+import Login from '../pages/Login/Login';
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigator() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="LandingPage" screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="LandingPage" component={LandingPage} />
+                <Stack.Screen name="Login" component={Login} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
