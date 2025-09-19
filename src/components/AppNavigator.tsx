aedffebcb2fdf6e5f640d780c7a126ed1b7fc71d
IvannEmmanuel <F0dG1ngD13@gmail.com>
2025-09-20 01:41:54 +0800

    refactor: added a screen for register, otp and face then a reigstrationProvider

--- src/components/AppNavigator.tsx
+++ src/components/AppNavigator.tsx
@@ -3,17 +3,24 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LandingPage from '../pages/LandingPage';
 import Login from '../pages/Login/Login';
 import Register from '../pages/Register/Register';
+import Otp from '../pages/Register/Otp';
+import Face from '../pages/Register/Face';
+import { RegistrationProvider } from '../hooks/RegistrationContext';
 
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
     return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName="LandingPage" screenOptions={{ headerShown: false }}>
-                <Stack.Screen name="LandingPage" component={LandingPage} />
-                <Stack.Screen name="Login" component={Login} />
-                <Stack.Screen name="Register" component={Register}/>
-            </Stack.Navigator>
-        </NavigationContainer>
+        <RegistrationProvider>
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName="LandingPage" screenOptions={{ headerShown: false }}>
+                    <Stack.Screen name="LandingPage" component={LandingPage} />
+                    <Stack.Screen name="Login" component={Login} />
+                    <Stack.Screen name="Register" component={Register} />
+                    <Stack.Screen name="Otp" component={Otp} />
+                    <Stack.Screen name="Face" component={Face} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </RegistrationProvider>
     );
 }
