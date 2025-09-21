2cb4d605e5c6ddd087d730032009b2799833166d
IvannEmmanuel <F0dG1ngD13@gmail.com>
2025-09-21 20:38:16 +0800

    refactor: added a FavoritesProvider for global

--- src/components/AppNavigator.tsx
+++ src/components/AppNavigator.tsx
@@ -9,24 +9,27 @@ import { RegistrationProvider } from '../hooks/RegistrationContext';
 import Home from '../pages/Screens/Home';
 import Dashboard from '../pages/Dashboard/Dashboard';
 import FavoriteModal from './FavoriteModal';
+import { FavoritesProvider } from './FavoritesContext';
 
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
     return (
-        <RegistrationProvider>
-            <NavigationContainer>
-                <Stack.Navigator initialRouteName="LandingPage" screenOptions={{ headerShown: false }}>
-                    <Stack.Screen name="LandingPage" component={LandingPage} />
-                    <Stack.Screen name="Login" component={Login} />
-                    <Stack.Screen name="Register" component={Register} />
-                    <Stack.Screen name="Otp" component={Otp} />
-                    <Stack.Screen name="Face" component={Face} />
-                    <Stack.Screen name="Home" component={Home} />
-                    <Stack.Screen name="Dashboard" component={Dashboard} />
-                    <Stack.Screen name="Favorites" component={FavoriteModal} />
-                </Stack.Navigator>
-            </NavigationContainer>
-        </RegistrationProvider>
+        <FavoritesProvider>
+            <RegistrationProvider>
+                <NavigationContainer>
+                    <Stack.Navigator initialRouteName="LandingPage" screenOptions={{ headerShown: false }}>
+                        <Stack.Screen name="LandingPage" component={LandingPage} />
+                        <Stack.Screen name="Login" component={Login} />
+                        <Stack.Screen name="Register" component={Register} />
+                        <Stack.Screen name="Otp" component={Otp} />
+                        <Stack.Screen name="Face" component={Face} />
+                        <Stack.Screen name="Home" component={Home} />
+                        <Stack.Screen name="Dashboard" component={Dashboard} />
+                        <Stack.Screen name="Favorites" component={FavoriteModal} />
+                    </Stack.Navigator>
+                </NavigationContainer>
+            </RegistrationProvider>
+        </FavoritesProvider>
     );
 }
