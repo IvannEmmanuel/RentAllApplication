84fec20fd6ba150ef402e58ec448b47c740ee402
IvannEmmanuel <F0dG1ngD13@gmail.com>
2025-10-22 21:51:24 +0800

    refactor: aspect ratio to whole screen

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -59,8 +59,6 @@ const Register = () => {
         const result = await ImagePicker.launchImageLibraryAsync({
             mediaTypes: ImagePicker.MediaTypeOptions.Images,
             allowsEditing: true,
-            aspect: [4, 3],
-            quality: 1,
         });
 
         if (!result.canceled) {
